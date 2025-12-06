e66e83159bd248012e42de5cd0c453e52dd77d67
junani5 <junani7@naver.com>
2025-12-06 17:07:43 +0900

    section 3 삭제

--- my-app/src/pages/MainPage/main/MainPage.jsx
+++ my-app/src/pages/MainPage/main/MainPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Section1 } from "../Section1/Section1";
 import { Section2 } from "../Section2/Section2";
-import { Section3 } from "../Section3/Section3";
 import { Section4 } from "../Section4/Section4";
 import { Footer } from "../../../components/Footer/Footer";
 import { Navbar } from "../../../components/Navbar/Navbar";
@@ -14,7 +13,6 @@ export const MainPage = () => {
       <Navbar/>
       <Section1 />
       <Section2 />
-      <Section3 />
       <Section4 />
       <Footer />
     </>
